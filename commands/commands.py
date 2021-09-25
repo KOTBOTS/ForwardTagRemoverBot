68068371e0eb27b5304ddccd345b3b264fb25dd5
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-09-25 18:19:48 +0530

    Update commands.py

--- commands/commands.py
+++ commands/commands.py
@@ -22,7 +22,7 @@ keyboard = [
  InlineKeyboardButton("Source Code", url=Config.SOURCE)
 ],
 [
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
+ InlineKeyboardButton("BOTS CHANNEL",url="https://t.me/KOT_BOTS")
 ]
 ]
 
